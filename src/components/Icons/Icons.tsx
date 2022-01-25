fe9d60fbb1c786cabd93465c97191fef747b35a5
Alson <Teemotmo@hotmail.com>
2022-01-25 18:25:05 +0800

    fixed upload icon

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -575,18 +575,36 @@ export const CloseIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 export const UploadIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
+    width="500" height="500" viewBox="0 0 500 500"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path 
-    fillRule="evenodd" 
-    clipRule="evenodd"
-    d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z" />
-    <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+    <defs>
+      <clipPath id="a">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+    </defs>
+    <g clipPath="url(#a)">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke="#FFF"
+        strokeWidth={14}
+        d="M87.624 299.512l-.065 44.736c-.033 22.076 17.865 40 39.941 40h240.124c22.076 0 40.023-17.924 40.052-40 0 0 .036-28.48.056-43.044"
+        fill="none"
+        display="block"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke="#FFF"
+        strokeWidth={13.2391}
+        d="M303.998 173.674l-55.672-55.677-56.024 56.025m55.998-55.503v215.003"
+        fill="none"
+        display="block"
+      />
+    </g>
   </svg>
 );
 
